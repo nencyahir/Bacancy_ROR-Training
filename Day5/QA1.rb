1a7f2ac6555fabe6eadcc4b4390ebdcb47ed8c22
KrupaKhant24 <117979907+KrupaKhant24@users.noreply.github.com>
2023-02-21 16:46:11 +0530

    Update QA1.rb

--- Day5/QA1.rb
+++ Day5/QA1.rb
@@ -7,12 +7,12 @@ birthday = DateTime.new(2002,1,24,2,30,0)
 puts "Current time is:" + Time.now.to_s
 puts"Birthday is: #{ birthday.to_time}"
 
-age_in_seconds = Time.now - birthday.to_time
-age_in_minutes = age_in_seconds / 60
-age_in_hours = age_in_minutes / 60
-age_in_days = age_in_hours / 24
-age_in_months = age_in_days / 30.44
-age_in_years = age_in_months / 12
+seconds = Time.now - birthday.to_time
+minutes = seconds / 60
+hours = minutes / 60
+days = hours / 24
+months = days / 30.44
+years = months / 12
 
 
-puts "Age is #{age_in_years.to_i} year #{(age_in_months % 12).to_i} month #{(age_in_days % 30.44).to_i} days #{(age_in_hours % 24).to_i} hours and #{(age_in_minutes % 60).to_i} minutes"
+puts "Age is #{years.to_i} year #{(months % 12).to_i} month #{(days % 30.44).to_i} days #{(hours % 24).to_i} hours and #{(minutes % 60).to_i} minutes"

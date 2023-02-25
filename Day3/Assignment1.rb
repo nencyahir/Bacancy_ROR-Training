e3008794062d0c97e833f53d36190ad391cfd696
nencyahir <125643703+nencyahir@users.noreply.github.com>
2023-02-25 10:53:23 +0530

    Update Assignment1.rb

--- Day3/Assignment1.rb
+++ Day3/Assignment1.rb
@@ -1,29 +1,28 @@
 EXPECTED_MINUTES_IN_OVEN=40
+CONSTANT_TWO=2
 
 class Lasanga 
+
 	puts "expected minutes is #{EXPECTED_MINUTES_IN_OVEN}"
  
-	def remaining_minutes_in_oven(t)
-	  result=EXPECTED_MINUTES_IN_OVEN-t
+	def remaining_minutes_in_oven(time)
+	  result=EXPECTED_MINUTES_IN_OVEN-time
 	  puts "remaining #{result}"
 	  return result
 	end
 
 	def preparation_time_in_minutes(layer)
-	  totallayer=layer * 2
+	  totallayer=layer * CONSTANT_TWO
 	  puts "Layer #{totallayer}"
 	  return totallayer
 	end
-
-
-	def total_time_in_minutes (time,layer)
-	  final_time = remaining_minutes_in_oven(time) + preparation_time_in_minutes(layer)
-	  print"Total Time:  #{final_time}"
-	end
+	
+       def total_time_in_minutes (time,layer)
+	  puts"Total Time:#{ final_time = remaining_minutes_in_oven(time) + preparation_time_in_minutes(layer)}"
+       end	
 end
 
-obj1=Lasanga.new
-
-obj1.remaining_minutes_in_oven(20)
-obj1.preparation_time_in_minutes(3)
-obj1.total_time_in_minutes(20,3)
+lasanga_obj=Lasanga.new
+lasanga_obj.remaining_minutes_in_oven(20)
+lasanga_obj.preparation_time_in_minutes(3)
+lasanga_obj.total_time_in_minutes(20,3)
